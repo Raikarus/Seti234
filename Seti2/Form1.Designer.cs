6ea5294ce87933806bcc26392ea04a8c6bc08d43
Raikarus <46867194+Raikarus@users.noreply.github.com>
2022-12-04 17:50:03 +0300

    Add files via upload

---
+++ Seti2/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Seti2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.Maximum = 1D;
+            chartArea1.AxisY.Minimum = -1D;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(788, 438);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 444);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 447);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Манчестерское";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 470);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(514, 444);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(274, 176);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "AMI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 528);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "NRZI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(133, 443);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Лаба2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(191, 443);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Лаба3";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(250, 443);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "Лаба4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(433, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Цвет";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 632);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Seti2/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Seti2
+{
+    public partial class Form1 : Form
+    {
+        string inputMessage = "";
+        string outputMessage = "";
+        string outputMessage2 = "";
+        string outputMessage22 = "";
+        string outputMessage3 = "";
+        string dopSymb = "";
+        bool tactSignal = true;
+        bool bufer = false;
+        bool tumbler = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool toBool(char c)
+        {
+            if (c == '1')
+                return true;
+            else
+                return false;
+        }
+
+        char toChar(bool c)
+        {
+            if (c)
+                return '1';
+            else
+                return '0';
+        }
+
+        string scrembl(string inp)
+        {
+            string outp = "";
+            for (int i = 0; i < 5; i++)
+            {
+                outp += inp[i];
+            }
+            outp += outp[0] ^ inp[5];
+            outp += outp[1] ^ inp[6];
+            outp += outp[2] ^ inp[7];
+            outp += outp[3] ^ inp[8];
+            outp += toChar(toBool(outp[4]) ^ toBool(outp[0]) ^ toBool(inp[9]));
+            outp += toChar(toBool(outp[5]) ^ toBool(outp[1]) ^ toBool(inp[10]));
+            for (int i = 11; i < inp.Length; i++)
+            {
+                outp += toChar(toBool(outp[i - 5]) ^ toBool(outp[i-9]) ^ toBool(outp[i-11]) ^ toBool(inp[i]));
+            }
+            return outp;
+        }
+
+        string descrembl(string inp)
+        {
+            string outp = "";
+            for (int i = 0; i < 5; i++)
+            {
+                outp += inp[i];
+            }
+            outp += inp[0] ^ inp[5];
+            outp += inp[1] ^ inp[6];
+            outp += inp[2] ^ inp[7];
+            outp += inp[3] ^ inp[8];
+            outp += toChar(toBool(inp[4]) ^ toBool(inp[0]) ^ toBool(inp[9]));
+            outp += toChar(toBool(inp[5]) ^ toBool(inp[1]) ^ toBool(inp[10]));
+            for (int i = 11; i < inp.Length; i++)
+            {
+                outp += toChar(toBool(inp[i - 5]) ^ toBool(inp[i-9]) ^ toBool(inp[i-11]) ^ toBool(inp[i]));
+            }
+            return outp;
+        }
+
+        void bass()
+        {
+            inputMessage = textBox1.Text;
+            outputMessage = "";
+            outputMessage2 = "";
+            outputMessage22 = "";
+            tumbler = false;
+            dopSymb = "";
+            foreach (var symb in inputMessage)
+            {
+                if (symb != ' ')
+                {
+                    dopSymb = BitConverter.ToString(Encoding.Default.GetBytes(symb.ToString()));
+                    
+                }
+                else
+                {
+                    if (!tumbler)
+                    {
+                        dopSymb = "00";
+                        tumbler = true;
+                    }
+                    else
+                    {
+                        dopSymb = "FF";
+                    }
+                }
+                outputMessage += dopSymb;
+                outputMessage2 += Convert.ToString(Convert.ToInt64(dopSymb, 16), 2).PadLeft(dopSymb.Length * 4, '0');
+            }
+            if (radioButton2.Checked)
+            {
+                string four;
+                string[] mas1 = new string[] { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" };
+                string[] mas2 = new string[] { "11110", "01001", "10100", "10101", "01010", "01011", "01110", "01111", "10010", "10011", "10110", "10111", "11010", "11011", "11100", "11101" };
+                for(int i = 3;i < outputMessage2.Length;i+=4)
+                {
+                    four = outputMessage2[i - 3].ToString() + outputMessage2[i - 2].ToString() + outputMessage2[i - 1].ToString() + outputMessage2[i].ToString();
+                    for(int j = 0; j < 16; j++)
+                    {
+                        if (four == mas1[j])
+                        {
+                            outputMessage22 += mas2[j];
+                            break;
+                        }
+                    }
+                }
+                outputMessage2 = outputMessage22;
+            }
+            else if (radioButton3.Checked)
+            {
+                string msg = outputMessage2 == descrembl(scrembl(outputMessage2)) ? "OK" : "ERROR";
+                MessageBox.Show(outputMessage2 + '\n' + descrembl(scrembl(outputMessage2))+ '\n' + msg);
+                outputMessage2 = scrembl(outputMessage2);
+            }
+            label1.Text = outputMessage2;
+            label2.Text = outputMessage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputMessage3 = "";
+            bass();
+            chart1.Series[0].Points.Clear();
+            foreach (var symb in outputMessage2)
+            {
+                if (symb == '0') bufer = false;
+                else bufer = true;
+                if (bufer)
+                {
+                    chart1.Series[0].Points.Add(1);
+                    chart1.Series[0].Points.Add(0);
+                    outputMessage3 +='1';
+                }
+                else
+                {
+                    chart1.Series[0].Points.Add(0);
+                    chart1.Series[0].Points.Add(1);
+                    outputMessage3 += '0';
+                }
+                tactSignal = !tactSignal;
+            }
+            richTextBox1.Text = outputMessage + '\n' + outputMessage2 + '\n' + outputMessage3 + '\n';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            outputMessage3 = "";
+            bool previousTrue = false;
+            bass();
+            chart1.Series[0].Points.Clear();
+            foreach (var symb in outputMessage2)
+            {
+                if (symb == '1')
+                {
+                    if(previousTrue)
+                    {
+                        chart1.Series[0].Points.Add(-1);
+                        outputMessage3+='1';
+                    }
+                    else
+                    {
+                        chart1.Series[0].Points.Add(1);
+                        outputMessage3 += '1';
+                    }
+                    previousTrue = !previousTrue;
+                }
+                else
+                {
+                    chart1.Series[0].Points.Add(0);
+                    outputMessage3 += '0';
+                }
+            }
+            richTextBox1.Text = outputMessage + '\n' + outputMessage2 + '\n' + outputMessage3 + '\n';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputMessage3 = "";
+            bool previousTact = false;
+            bass();
+            chart1.Series[0].Points.Clear();
+            foreach (var symb in outputMessage2)
+            {
+                if (symb == '1')
+                {
+                    previousTact = !previousTact;
+                }
+                if (previousTact)
+                {
+                    chart1.Series[0].Points.Add(1);
+                    outputMessage3 += '1';
+                }
+                else
+                {
+                    chart1.Series[0].Points.Add(0);
+                    outputMessage3 += '0';
+                }
+            }
+            richTextBox1.Text = outputMessage + '\n' + outputMessage2 + '\n' + outputMessage3 + '\n';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            chart1.Series[0].Color = colorDialog1.Color;
+        }
+    }
+}
